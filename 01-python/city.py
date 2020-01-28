3f09d5aa54cd39aa25e4720e517830af9273f5e1
Zhitong Su <szt1112@bu.edu>
2020-01-28 14:48:32 -0500

    delete

--- 01-python/city.py
+++ 01-python/city.py
@@ -1,4 +1,4 @@
-from safety.safety import draw_safety
+rom safety.safety import draw_safety
 from leisure.leisure import draw_leisure
 from outdoors.outdoors import draw_outdoors
 from education.education import draw_education
